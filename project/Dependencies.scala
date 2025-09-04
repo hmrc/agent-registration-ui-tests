7d6f4d14a0085d69d3e74c18ffeb62e6bc33a881
MarkJamesB <markjbennett1@gmail.com>
2025-09-04 10:29:27 +0100

    Removed duplicated dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,10 +3,7 @@ import sbt.*
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"            %% "ui-test-runner" % "0.48.0" % Test,
-    "org.scalatest"          %% "scalatest"      % "3.2.19" % Test,
-    "org.seleniumhq.selenium" % "selenium-java"  % "4.23.0" % Test,
-    "com.typesafe"            % "config"         % "1.4.3"
+    "uk.gov.hmrc"            %% "ui-test-runner" % "0.48.0" % Test
   )
 
 }
