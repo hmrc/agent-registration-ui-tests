08921ccb414937a5b05ee97f1fa205ff12570f99
MarkJamesB <markjbennett1@gmail.com>
2025-11-25 09:47:44 +0000

    APB-10319-10320-Capture-Agent-Details (#17)

---
+++ src/test/scala/uk/gov/hmrc/ui/specs/ukbased/partnerships/limited_liability_partnership/application/agentdetails/AgentDetailsSpec.scala
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.specs.ukbased.partnerships.limited_liability_partnership.application.agentdetails
+
+import uk.gov.hmrc.ui.flows.ukbased.partnerships.limited_liability_partnership.application.businessdetails.BusinessDetailsFlow
+import uk.gov.hmrc.ui.flows.ukbased.partnerships.limited_liability_partnership.application.agentdetails.AgentDetailsFlow
+import uk.gov.hmrc.ui.flows.ukbased.partnerships.limited_liability_partnership.application.contactdetails.ContactDetailsFlow
+import uk.gov.hmrc.ui.pages.agentregistration.ukbased.partnerships.limited_liability_partnership.application.TaskListPage
+import uk.gov.hmrc.ui.pages.agentregistration.ukbased.partnerships.limited_liability_partnership.application.agentdetails.CheckYourAnswersPage
+import uk.gov.hmrc.ui.pages.agentregistration.ukbased.partnerships.limited_liability_partnership.application.agentdetails.WhatBusinessNamePage
+import uk.gov.hmrc.ui.pages.agentregistration.ukbased.partnerships.limited_liability_partnership.application.agentdetails.WhatTelephoneNumberPage
+import uk.gov.hmrc.ui.specs.BaseSpec
+
+class AgentDetailsSpec
+extends BaseSpec:
+
+  Feature("Complete Business Name section"):
+    Scenario("User selects existing details", HappyPath):
+
+      val stubbedSignInData = BusinessDetailsFlow
+        .WhenHasNoOnlineAgentAccount
+        .runFlow()
+
+      ContactDetailsFlow
+        .WhenMultiNameMatch
+        .runFlow(stubbedSignInData)
+
+      AgentDetailsFlow
+        .WhenUsingProvidedOptions
+        .runFlow()
+      TaskListPage.assertAgentServicesAccountDetailsStatus("Incomplete") // TODO change to complete once email and correspondence addresses added
+
+    Scenario("User enters all custom values", HappyPath):
+
+      val stubbedSignInData = BusinessDetailsFlow
+        .WhenHasNoOnlineAgentAccount
+        .runFlow()
+
+      ContactDetailsFlow
+        .WhenMultiNameMatch
+        .runFlow(stubbedSignInData)
+
+      AgentDetailsFlow
+        .WhenUsingCustomValues
+        .runFlow()
+      TaskListPage.assertAgentServicesAccountDetailsStatus("Incomplete") // TODO change to complete once email and correspondence addresses added
+
+    Scenario("User mixes provided and custom options", HappyPath):
+
+      val stubbedSignInData = BusinessDetailsFlow
+        .WhenHasNoOnlineAgentAccount
+        .runFlow()
+
+      ContactDetailsFlow
+        .WhenMultiNameMatch
+        .runFlow(stubbedSignInData)
+
+      AgentDetailsFlow.startJourney()
+      AgentDetailsFlow.selectBusinessName("My Custom LLP")
+      AgentDetailsFlow.selectTelephoneNumber("hmrc provided")
+
+      CheckYourAnswersPage.assertPageIsDisplayed()
+      CheckYourAnswersPage.assertSummaryRow("Business name for clients", "My Custom LLP")
+      CheckYourAnswersPage.assertSummaryRow("Telephone number", "01234567890")
+      CheckYourAnswersPage.clickContinue()
+
+      TaskListPage.assertPageIsDisplayed()
+      TaskListPage.assertAgentServicesAccountDetailsStatus("Incomplete") // TODO change to complete once email and correspondence addresses added
+
+    Scenario("Change Business Name from Check Your Answers page", HappyPath):
+
+      val stubbedSignInData = BusinessDetailsFlow
+        .WhenHasNoOnlineAgentAccount
+        .runFlow()
+
+      ContactDetailsFlow
+        .WhenMultiNameMatch
+        .runFlow(stubbedSignInData)
+
+      AgentDetailsFlow
+        .runToCheckYourAnswers
+        .runFlow()
+
+      CheckYourAnswersPage.clickChangeFor("Business name for clients")
+
+      WhatBusinessNamePage.assertPageIsDisplayed()
+      WhatBusinessNamePage.selectSomethingElse()
+      WhatBusinessNamePage.enterCustomName("Updated LLP Name")
+      WhatBusinessNamePage.clickContinue()
+
+      CheckYourAnswersPage.assertPageIsDisplayed()
+      CheckYourAnswersPage.assertSummaryRow("Business name for clients", "Updated LLP Name")
+
+    Scenario("Change Telephone Number from Check Your Answers page", HappyPath):
+
+      val stubbedSignInData = BusinessDetailsFlow
+        .WhenHasNoOnlineAgentAccount
+        .runFlow()
+
+      ContactDetailsFlow
+        .WhenMultiNameMatch
+        .runFlow(stubbedSignInData)
+
+      AgentDetailsFlow
+        .runToCheckYourAnswers
+        .runFlow()
+
+      CheckYourAnswersPage.clickChangeFor("Telephone number")
+
+      WhatTelephoneNumberPage.assertPageIsDisplayed()
+      WhatTelephoneNumberPage.selectSomethingElse()
+      WhatTelephoneNumberPage.enterOtherTelephoneNumber("07777799999")
+      WhatTelephoneNumberPage.clickContinue()
+
+      CheckYourAnswersPage.assertPageIsDisplayed()
+      CheckYourAnswersPage.assertSummaryRow("Telephone number", "07777799999")
