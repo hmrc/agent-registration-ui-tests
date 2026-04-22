b36a72313a3b3010b67e9453bea57a15eafdea67
soloshah <38477922+soloshah@users.noreply.github.com>
2026-04-22 09:53:44 +0100

    updated ui test runner version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "ui-test-runner"              % "0.53.0" % Test,
+    "uk.gov.hmrc"       %% "ui-test-runner"              % "0.54.0" % Test,
     "org.playframework" %% "play-test"                   % "3.0.10" % Test,
     "org.mongodb.scala"  % "mongo-scala-driver_2.13"     % "5.6.4" % Test
   )
