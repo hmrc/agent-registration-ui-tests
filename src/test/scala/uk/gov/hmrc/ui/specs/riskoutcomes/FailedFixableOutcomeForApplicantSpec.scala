3941175c8075a49e333cad120e749f621b07c298
soloshah <38477922+soloshah@users.noreply.github.com>
2026-06-22 11:52:05 +0100

    rebase main and restructure risking tests

--- src/test/scala/uk/gov/hmrc/ui/specs/riskoutcomes/FailedFixableOutcomeForApplicantSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/riskoutcomes/FailedFixableOutcomeForApplicantSpec.scala
@@ -14,9 +14,9 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.ui.specs.ukbased.partnerships.general_partnership.riskingResults.riskOutcome
+package uk.gov.hmrc.ui.specs.riskoutcomes
 
-import uk.gov.hmrc.ui.domain.BusinessType.GeneralPartnership
+import uk.gov.hmrc.ui.domain.BusinessType.{GeneralPartnership, SoleTrader}
 import uk.gov.hmrc.ui.flows.common.application.FastForwardLinks
 import uk.gov.hmrc.ui.flows.common.application.FastForwardLinks.ApplicationProgress.AgentStandards
 import uk.gov.hmrc.ui.flows.common.application.declaration.DeclarationFlow
@@ -34,8 +34,60 @@ extends BaseSpec:
 
   Feature("Applicant FailedFixable List Page"):
     Scenario(
+      "Sole Trader Owner sees FailedFixable Outcome Page after sign in",
+      TagSmokeTests
+    ):
+
+      val stubbedSignInData = FastForwardLinks
+        .FastForward
+        .runFlow(AgentStandards, SoleTrader)
+
+      ProvideIndividualDetailsFlow
+        .ProvideIndividualDetailsSoleTrader
+        .runFlow(
+          stubbedSignInData,
+          listProgress.complete,
+          fastForwardUsed = true
+        )
+
+      DeclarationFlow.AcceptDeclaration.runFlow(SoleTrader, fastForwardUsed = true)
+
+      ApplicationSubmittedPage.assertPageIsDisplayed()
+
+      val applicationReference = ApplicationSubmittedPage.getApplicationReference
+      MongoHelper
+        .findByApplicationReference(applicationReference)
+        .getOrElse(throw new AssertionError(s"No Mongo record found for reference: $applicationReference"))
+
+      MongoHelper.simulateFixableRiskingOutcome(applicationReference)
+
+      RiskingOutcomeFlow
+        .SignInAsApplicantAfterRiskingOutcome
+        .runFlow(stubbedSignInData)
+
+      ApplicationSubmittedPage.assertPageIsDisplayed()
+      ApplicationSubmittedPage.assertPageHeadingContains("ST Name ST Lastname")
+
+      val outcomeDoc = MongoHelper
+        .findByApplicationReference(applicationReference)
+        .getOrElse(throw new AssertionError(s"No Mongo record found for reference: $applicationReference"))
+      MongoHelper.getTopLevelString(outcomeDoc, "riskingFileName") shouldBe "any-old.txt"
+
+      val failures = MongoHelper.getEntityRiskingFailures(outcomeDoc)
+      println(s"Failures: $failures")
+      failures should not be empty
+
+      val failureTypes = failures.map(f => MongoHelper.getNestedString(f, "type"))
+      failureTypes should contain allOf("_4._1", "_4._3")
+
+      ApplicationSubmittedPage.assertOutcomeDescriptionContainsAll(
+        "ST Name ST Lastname will not be given an agent services account on this occasion",
+        "the application will be deleted on 3 August 2026 to comply with our data retention policy"
+      )
+
+    Scenario(
       "General Partnership sees FailedFixable Outcome Page when partner have individual failures",
-      TagGeneralPartnership
+      TagSmokeTests
     ):
 
       val stubbedSignInData = FastForwardLinks
