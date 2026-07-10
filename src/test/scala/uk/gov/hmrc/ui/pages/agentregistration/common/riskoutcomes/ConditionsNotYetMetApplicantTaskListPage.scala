301034ec108401a726f234cd4bf893a9fbe96a3b
soloshah <38477922+soloshah@users.noreply.github.com>
2026-07-10 09:49:46 +0100

    added test for applicant task list

--- src/test/scala/uk/gov/hmrc/ui/pages/agentregistration/common/riskoutcomes/ConditionsNotYetMetApplicantTaskListPage.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/agentregistration/common/riskoutcomes/ConditionsNotYetMetApplicantTaskListPage.scala
@@ -32,6 +32,12 @@ extends BasePage:
   private val individualFailuresLink = By.cssSelector("a[aria-describedby='individualFailures-1-status']")
   private val selfAssessmentReturnsLink = By.cssSelector("a[aria-describedby='entityFailures-1-status']")
   private val selfAssessmentMissingReturns = By.id("entityFailures-1-status")
+  private val vatMissingReturns = By.id("entityFailures-2-status")
+  private val pageTitleHeading = By.cssSelector("h1.govuk-heading-l")
+  private val amlsSubHeading = By.xpath("(//h2[contains(@class, 'govuk-heading-m')])[2]")
+  private val businessIssuesSubHeading = By.xpath("(//h2[contains(@class, 'govuk-heading-m')])[3]")
+  private val resubmitApplicationSubHeading = By.xpath("(//h2[contains(@class, 'govuk-heading-m')])[4]")
+  private val saveAndComeBackLaterButton = By.cssSelector("a.govuk-button--secondary[href='/agent-registration/conditions-not-yet-met/save-and-come-back-later']")
 
   def clickIndividualFailuresLink(): Unit = click(individualFailuresLink)
 
@@ -50,3 +56,16 @@ extends BasePage:
   def clickOnSelfAssessmentReturnsLink(): Unit = click(selfAssessmentReturnsLink)
 
   def assertSelfAssessmentMissingReturns(expectedStatus: String): Unit = getText(selfAssessmentMissingReturns) shouldBe expectedStatus
+
+  def assertTaskListTitleHeading(expected: String): Unit = getText(pageTitleHeading) shouldBe (expected)
+
+  def assertTaskListAmlsSubeHeading(expected: String): Unit = getText(amlsSubHeading) shouldBe (expected)
+  
+  def assertTaskListBusinessIssuesSubHeading(expected: String): Unit = getText(businessIssuesSubHeading) shouldBe (expected)
+  
+  def assertTaskListResubmitApplicationSubHeading(expected: String): Unit = getText(resubmitApplicationSubHeading) shouldBe (expected)
+
+  def assertVatMissingReturns(expectedStatus: String): Unit = getText(vatMissingReturns) shouldBe expectedStatus
+
+  def clickSaveAndComeBackLaterButton(): Unit = click(saveAndComeBackLaterButton)
+   
---
+++ src/test/scala/uk/gov/hmrc/ui/specs/riskoutcomes/FailedFixableOutcomeForApplicantFailureTasklistSpec.scala
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.specs.riskoutcomes
+
+import org.mongodb.scala.Document
+import uk.gov.hmrc.ui.domain.BusinessType.LLP
+import uk.gov.hmrc.ui.flows.common.application.FastForwardLinks
+import uk.gov.hmrc.ui.flows.common.application.FastForwardLinks.ApplicationProgress.Declaration
+import uk.gov.hmrc.ui.flows.common.application.riskingOutcome.RiskingOutcomeFlow
+import uk.gov.hmrc.ui.pages.agentregistration.common.application.ApplicationSubmittedPage
+import uk.gov.hmrc.ui.pages.agentregistration.common.riskoutcomes.{ApplicationStatusPage, ConditionsNotYetMetApplicantTaskListPage, ConditionsNotYetMetEntityFailureDetailsV41Page, SaveAndComeBackLater}
+import uk.gov.hmrc.ui.specs.BaseSpec
+import uk.gov.hmrc.ui.utils.MongoHelper
+import uk.gov.hmrc.ui.utils.MongoHelper.IndividualRiskingOutcome
+
+class FailedFixableOutcomeForApplicantFailureTasklistSpec
+extends BaseSpec:
+
+  Feature("Applicant Failure Details Page"):
+    Scenario(
+      "LLP user sees FailedFixable Details Page and click Yes/No variety for Self Assessment returns issue",
+      TagFixableFailures
+    ):
+      val stubbedSignInData = FastForwardLinks
+        .FastForward
+        .runFlow(Declaration, LLP)
+
+      ApplicationSubmittedPage.assertPageIsDisplayed()
+
+      val applicationReference = ApplicationSubmittedPage.getApplicationReference
+      MongoHelper
+        .findByApplicationReference(applicationReference)
+        .getOrElse(throw new AssertionError(s"No Mongo record found for reference: $applicationReference"))
+
+      val amlsFixes = Seq(
+        Document(
+          "failure" -> Document("type" -> "_3._1"),
+          "amlsDetails" -> Document(
+            "supervisoryBody" -> "HMRC",
+            "amlsRegistrationNumber" -> "XAML00000123456"
+          ),
+          "type" -> "EntityFix._3.AmlsFix"
+        ),
+        Document(
+          "type" -> "EntityFix._4._1"
+        ),
+        Document(
+          "type" -> "EntityFix._4._3"
+        )
+      )
+
+      MongoHelper.insertRiskingOutcomeToAgentApplicationWithAmlsDetails(
+        applicationReference = applicationReference,
+        riskingCompletedDate = "2026-06-18",
+        outcome = "FailedFixable",
+        correctiveActionExpiryDate = "2026-08-17",
+        fixes = amlsFixes
+      )
+
+      // Insert two individuals. One with all actions confirmed, one with some actions unconfirmed
+      MongoHelper.insertRiskingOutcomeIndividualsToAgentApplication(
+        applicationReference = applicationReference,
+        outcomesByIndividualName = Map(
+          "Steve Austin" -> IndividualRiskingOutcome(
+            fixes = Seq.empty
+          ),
+          "Beverly Hills" -> IndividualRiskingOutcome(
+            fixes = Seq.empty
+          )
+        )
+      )
+
+      RiskingOutcomeFlow
+        .SignInAsApplicantAfterRiskingOutcome
+        .runFlow(stubbedSignInData)
+
+      ApplicationSubmittedPage.assertPageIsDisplayed()
+      ApplicationSubmittedPage.assertConfirmationTitleHeading("Test Partnership does not meet the registration conditions yet")
+
+      // Click the "View actions to take" button to navigate to the Conditions Not Met Task List page
+      ApplicationStatusPage.clickViewActionsToTakeButton()
+      ConditionsNotYetMetApplicantTaskListPage.assertPageIsDisplayed()
+      ConditionsNotYetMetApplicantTaskListPage.assertTaskListTitleHeading("Take action: Test Partnership has not met the registration conditions")
+      ConditionsNotYetMetApplicantTaskListPage.assertTaskListAmlsSubeHeading("Anti-money laundering supervision")
+      ConditionsNotYetMetApplicantTaskListPage.assertTaskListBusinessIssuesSubHeading("Issues with the business")
+      ConditionsNotYetMetApplicantTaskListPage.assertTaskListResubmitApplicationSubHeading("Resubmit application")
+
+      // Assert that the status of the tasks is "Incomplete"
+      ConditionsNotYetMetApplicantTaskListPage.assertAmlsDetailsStatus("Incomplete")
+      ConditionsNotYetMetApplicantTaskListPage.assertSelfAssessmentMissingReturns("Incomplete")
+      ConditionsNotYetMetApplicantTaskListPage.assertVatMissingReturns("Incomplete")
+
+      // Click the "Save and Come Back Later" button to navigate to the Save and Come Back Later page
+      ConditionsNotYetMetApplicantTaskListPage.clickSaveAndComeBackLaterButton()
+      SaveAndComeBackLater.assertPageIsDisplayed()
